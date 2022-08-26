f2dbbe9adcac0e56273e5bdd973bc40d037191bd
carvss <davi.carvalho@edu.unifil.br>
2022-08-25 23:25:29 -0300

    diferença

---
+++ exercícios/difference.py
@@ -0,0 +1,6 @@
+a = float(input("Informe o primeiro valor:"))
+b = float(input("Informe o segundo valor:"))
+c = float(input("Informe o terceiro valor:"))
+d = float(input("Informe o quarto valor:"))
+diferenca = a * b - c * d
+print("DIFERENCA = ", diferenca)

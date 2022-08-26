534d875413785bcecdc12aa1535e2af31536e6c7
carvss <davi.carvalho@edu.unifil.br>
2022-08-25 23:38:03 -0300

    salário

---
+++ exercícios/salary.py
@@ -0,0 +1,6 @@
+n = float(input("Informe o número do funcionario:"))
+wh = float(input("Informe o número de horas trabalhadas no mês:"))
+ph = float(input("Informe o valor recebido por hora de trabalho:"))
+salary = wh * ph
+print("NUMBER =", n)
+print("SALARY = U$", salary)
